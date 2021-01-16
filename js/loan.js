50c04452d4c64569e5dd6a90980d5c5c393a4887
alandres92 <aldenlandres@gmail.com>
2021-01-16 13:55:33 -0500

    first commit

---
+++ js/loan.js
@@ -0,0 +1,11 @@
+function startOver() {
+    document.getElementById("loanAmt").value="";
+    document.getElementById("months").value="";
+    document.getElementById("rate").value="";
+    document.getElementById("extra").value="";
+
+    document.getElementById("loanInfo").innerHTML="";
+}
+
+
+
